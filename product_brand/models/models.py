be5a1cc662cf5b2f2bbd5b3288bddec8056b446d
Jawaid94 <jawaid94@outlook.com>
2020-05-14 17:21:08 +0500

    add new module product brand

---
+++ product_brand/models/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ProductBrand(models.Model):
+    _inherit = 'product.template'
+
+    brand_id = fields.Many2one('product.brand', string='Brand')
+
+
+class BrandProduct(models.Model):
+    _name = 'product.brand'
+
+    name = fields.Char(String="Name")
